a02713572ede40d00da2756910ca06ca79035a6b
wafaa farrag <w.farag@speedi.sa>
2023-10-16 23:29:01 +0300

    api

---
+++ VideoShow/API/BaseAPI.swift
@@ -0,0 +1,8 @@
+//
+//  BaseAPI.swift
+//  VideoShow
+//
+//  Created by Wafaa Farag on 16/10/2023.
+//
+
+import Foundation
---
+++ VideoShow/Models/Link.swift
@@ -0,0 +1,8 @@
+//
+//  Link.swift
+//  VideoShow
+//
+//  Created by Wafaa Farag on 16/10/2023.
+//
+
+import Foundation
